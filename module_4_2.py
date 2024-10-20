f3fea9ef14598684774bbd91185292ddcb222be5
bochar68 <bochar.dg@gmail.com>
2024-10-20 16:11:30 +0300

    работа по уроку "Пространство имен."

---
+++ module_4_2.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print(f"Я в области видимости функции test_function")
+    inner_function()
+    print('************************')
+    return inner_function
+test_function()()
